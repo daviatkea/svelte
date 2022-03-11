932f7c84b517d59481318f0c79758d4a373a9d57
Dannie Vinther <davi@kea.dk>
2022-03-11 07:41:43 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Button from "./Button.svelte";
+</script>
+
+<h2>Buttons</h2>
+
+<section>
+  <h3>Default</h3>
+  <Button>Default knap</Button>
+  <pre><code>{'<Button type="primary">Første knap</Button>'}</code></pre>
+</section>
+
+<Button type="primary" inversed={true}>Første knap</Button>
+<Button type="secondary">Første knap</Button>
+<Button type="secondary" inversed={true}>Første knap</Button>
---
+++ src/Button.svelte
@@ -0,0 +1,36 @@
+<script>
+  export let type = "default";
+  export let inversed = false;
+</script>
+
+<button class:inversed class={type}><slot /></button>
+
+<style>
+  button {
+    border: none;
+    color: var(--primary-font-color);
+    background: var(--default-color);
+    padding: 1rem;
+    border-radius: 12px;
+    transition: all 0.2s;
+    cursor: pointer;
+  }
+  .primary {
+    background: var(--primary-color);
+  }
+  .primary:hover {
+    color: var(--primary-color);
+    background: var(--primary-font-color);
+  }
+  .secondary {
+    background: var(--secondary-color);
+  }
+  .primary.inversed {
+    color: var(--primary-color);
+    background: var(--primary-font-color);
+  }
+  .secondary.inversed {
+    color: var(--secondary-color);
+    background: var(--primary-font-color);
+  }
+</style>
